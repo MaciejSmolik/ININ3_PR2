df0e1283ad1677040fc806ef37dfa884e2af3928
ratengut72@gmail.com <ratengut72@gmail.com>
2022-08-17 20:13:50 +0200

    add mandatory fields with getters and setters #0

--- src/main/java/com/ratengut72/creatures/Human.java
+++ src/main/java/com/ratengut72/creatures/Human.java
@@ -1,4 +1,23 @@
 package com.ratengut72.creatures;
 
 public class Human {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- src/main/java/com/ratengut72/creatures/Phone.java
+++ src/main/java/com/ratengut72/creatures/Phone.java
@@ -1,4 +1,23 @@
 package com.ratengut72.creatures;
 
 public class Phone {
+
+    private String name;
+    private String model;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
